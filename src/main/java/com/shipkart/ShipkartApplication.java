962251649ef2ae0bca3b1a479b90d81daa16fafe
sakshishirsath <sakshishirsath8386@gmail.com>
2022-11-21 13:38:16 +0530

    First commit

---
+++ src/main/java/com/shipkart/ShipkartApplication.java
@@ -0,0 +1,13 @@
+package com.shipkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shipkart/ShipkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShipkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
